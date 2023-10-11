f0e89eeef1fd7c826064eab4d69755a9668ca1cf
thegreyclover <nafisah.o.isiaq@gmail.com>
2023-10-11 12:46:30 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,24 +1,13 @@
-
 #include <stdio.h>
-
 /**
- * main - program that prints the alphabet in lowercase and upper case
- *
- * Return: Always 0.
- */
-
+* main - prints all single digit numbers
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-
-	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
-	}
-
-	putchar('\n');
-
-	return (0);
+int i;
+for (int i = 0; i < 10; i++)
+printf("%d\n", i);
+return (0);
 }
