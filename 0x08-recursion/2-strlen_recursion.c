cb16c7aef46a0f8ab4cb24353c4fbd7c7d0f55fa
thegreyclover <nafisah.o.isiaq@gmail.com>
2023-10-26 13:43:58 -0700

    Why is it so important to dream?

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen_recursion - function that returns the length of a string
+* @s: the string to be measured
+* Return: Length of the string
+*/
+int _strlen_recursion(char *s)
+{
+int longit = 0;
+if (*s)
+{
+longitt++;
+longit += _strlen_recursion(s + 1);
+}
+return (longit);
+}
