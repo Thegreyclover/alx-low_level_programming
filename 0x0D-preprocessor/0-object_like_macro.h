791fc751eca5421b6a485419fb0b07f9fe8e3b42
Thegreyvlover <nafisah.isiaq@gmail.com>
2023-11-19 11:02:50 -0800

    object like macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+* OBJECT_LIKE_MACRO_H - Header guard to prevent multiple inclusions.
+* SIZE: Macro representing a constant value of 1024.
+*/
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,8 @@
+#ifndef PI_H
+#define PI_H
+/**
+* PI_H - Header guard to prevent multiple inclusions.
+* PI: Macro representing pi (π) with an approximation.
+*/
+#define PI 3.14159265359
+#endif
